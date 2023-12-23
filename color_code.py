f904c8a2a11e03d841258ae00369400ba01471b3
Hawkeyezz <clintonetuk@gmail.com>
2023-12-23 08:18:46 +0100

    Extracting color and code from image

---
+++ color_code.py
@@ -0,0 +1,20 @@
+from PIL import Image
+from collections import Counter
+
+
+# Open the image
+image = Image.open('input.png')
+
+# Convert the image to a list of RGB tuples
+pixels = list(image.getdata())
+
+# Use Counter to count the occurrences of each color
+color_counts = Counter(pixels)
+
+# Get the 6 most common colors
+top_colors = color_counts.most_common(6)
+
+# Print the extracted colors and their counts
+for i, (color, count) in enumerate(top_colors):
+    color_block = "\033[48;2;{};{};{}m    \033[0m".format(color[0], color[1], color[2])
+    print(f"Color {i + 1}: {color_block} RGB: {color} - Count: {count}")
